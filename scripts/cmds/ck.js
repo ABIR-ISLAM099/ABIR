61fa637fa953822bfff70eed1b17984290b7d3e8
ABIR ISLAM <abirislam099@outlook.com>
2024-11-02 12:51:59 +0600

    Update ck.js

--- scripts/cmds/ck.js
+++ scripts/cmds/ck.js
@@ -1,3 +1,5 @@
+const axios = require('axios'); // Ensure you have axios imported
+
 module.exports.config = {
     name: "noprefix",
     aliases: ["repeat"],
@@ -11,22 +13,24 @@ module.exports.config = {
 };
 
 module.exports.onChat = async function ({ event, args, message }) {
-
     const msg = args.join(" ");
 
     if (!msg) {
         return message.reply("Please provide a message.");
-        
+    }
+
     try {
         const messageContent = event.text;
-        if(messageContent.startsWith("ck")){
-    const apiUrl = `https://www.noobs-api.000.pe/dipto/baby?text=${encodeURIComponent(msg)}`;
-        const response = await axios.get(apiUrl);
-        const data = response.data.reply;
+        if (messageContent.startsWith("ck")) {
+            const apiUrl = `https://www.noobs-api.000.pe/dipto/baby?text=${encodeURIComponent(msg)}`;
+            const response = await axios.get(apiUrl);
+            const data = response.data.reply;
 
-        await message.reply(data);
-   }
+            await message.reply(data);
+        } else {
+            await message.reply(msg); // Echo back the message if it doesn't start with "ck"
+        }
     } catch (error) {
-        message.reply(error.message)
+        message.reply(error.message);
     }
 };
